40de5c33587a2b86fef56739f0e98a93b130eeac
RafaelYahKoh <rafael_yah_koh@hotmail.com>
2016-03-16 22:26:19 -0500

    Added files via upload

---
+++ ProyectoProgramacion/cuadros.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoProgramacion
+{
+    class cuadros
+    {
+        public void cuadro(int height, int width, int x, int y)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.SetCursorPosition(x + j, y + i);
+                    Console.WriteLine("█");
+                }
+            }
+        }
+        public void marco()
+        {
+           
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.SetCursorPosition(1, 1);
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("\t\t\t\t\t\t\t\t      ");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(" _ ");
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(" ⌂ ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" X ");
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(2, 2);
+            Console.Write("\t\t\t\t\t\t\t\t\t\t");
+            Console.SetCursorPosition(2, 3);
+            Console.Write("\t\t\t\t\t\t\t\t\t\t");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.SetCursorPosition(32, 2);
+            Console.WriteLine("Laboratory System");
+            for (int i = 0; i < 78; i++)
+            {
+                Console.SetCursorPosition(1 + i, 28);
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" ");
+            }
+            for (int i = 0; i < 28; i++)
+            {
+                Console.SetCursorPosition(1, 1 + i);
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" ");
+            }
+            for (int i = 0; i < 28; i++)
+            {
+                Console.SetCursorPosition(79, 1 + i);
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" ");
+            }
+          
+
+        }
+        public void line(int width,int x,int y)
+        {
+          
+            for (int i = 0; i < width; i++)
+            {
+                Console.SetCursorPosition(x+i, y);
+                Console.WriteLine("_");
+            }
+        }
+    }
+}
---
+++ ProyectoProgramacion/main.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ProyectoProgramacion
+{
+    class main:cuadros
+    {
+        public void title() {
+            int x = 16;
+            Console.SetCursorPosition(x,5);
+            Console.WriteLine("╦  ╔═╗╔╗ ╔═╗╦═╗╔═╗╔╦╗╔═╗╦═╗╦ ╦  ╔═╗╦ ╦╔═╗╔╦╗╔═╗╔╦╗");
+            Console.SetCursorPosition(x, 6);
+            Console.WriteLine("║  ╠═╣╠╩╗║ ║╠╦╝╠═╣ ║ ║ ║╠╦╝╚╦╝  ╚═╗╚╦╝╚═╗ ║ ║╣ ║║║");
+            Console.SetCursorPosition(x, 7);
+            Console.WriteLine("╩═╝╩ ╩╚═╝╚═╝╩╚═╩ ╩ ╩ ╚═╝╩╚═ ╩   ╚═╝ ╩ ╚═╝ ╩ ╚═╝╩ ╩");
+        }
+        public main()
+        {
+            ConsoleKeyInfo op;
+           
+            do
+            {
+                Console.SetWindowSize(81, 30);
+                Console.BufferHeight = Console.WindowHeight;
+                Console.BufferWidth = Console.WindowWidth;
+                marco();
+                Console.BackgroundColor = ConsoleColor.Black;
+                title();
+                //Height: ancho
+                //Width: Largo
+                //x = posicion en x
+                //Y = posicion en y
+                //CUADRO DE ALUMNO;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                cuadro(5, 20, 7, 10);
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(11, 12);
+                Console.WriteLine("   ALUMNO   ");
+                line(9, 12, 13);
+                //CUADRO DE ADMINISTRADOR
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                cuadro(5, 21, 31, 10);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(32, 12);
+                Console.WriteLine("   ADMINISTRADOR  ");
+                line(15, 34, 13);
+
+
+                //CUADRO DE REGISTRO
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                cuadro(5, 20, 55, 10);
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(57, 12);
+                Console.WriteLine(" REGISTRO ALUMNO ");
+                line(17, 57, 13);
+
+
+                //CUADRO DE AVISO
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                cuadro(8, 68, 7, 18);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(39, 19);
+                Console.WriteLine("AVISOS");
+                StreamReader arhivo = new StreamReader("C:\\Windows\\Temp\\BD\\aviso.txt");
+                int con = 0;
+                while (true)
+                {
+                    string line = arhivo.ReadLine();
+                    Console.SetCursorPosition(9, 21 + con);
+                    Console.WriteLine(line);
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    con++;
+                }
+                arhivo.Close();
+                //Pide una tecla y si presiona escape se cierra la ventana
+                op = Console.ReadKey();
+            } while (op.Key != ConsoleKey.Escape);
+        }
+    }
+}
